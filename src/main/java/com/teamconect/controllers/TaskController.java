6458f92b7fbf9e8cdba0180043f29bda41669024
SarAlbN1 <sara.albarracin.ns@gmail.com>
2024-10-27 12:24:13 -0500

    Crear método en TaskController para actualizar progreso de tarea.
    Closes #30

--- src/main/java/com/teamconect/controllers/TaskController.java
+++ src/main/java/com/teamconect/controllers/TaskController.java
@@ -1,12 +1,14 @@
 package com.teamconect.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.teamconect.dtos.TaskDTO;
 import com.teamconect.models.Task;
 import com.teamconect.services.TaskService;
 
@@ -18,8 +20,12 @@ public class TaskController {
     private TaskService taskService;
 
     @PostMapping("/assign")
-    public Task assignTask(@RequestBody TaskDTO taskDTO) {
-        Task task = new Task(taskDTO.getTitle(), taskDTO.getDescription(), taskDTO.getAssignedUserId(), taskDTO.getStatus());
+    public Task assignTask(@RequestBody Task task) {
         return taskService.assignTask(task);
+    }
+
+    @PutMapping("/update-status/{taskId}")
+    public Task updateTaskStatus(@PathVariable String taskId, @RequestParam String status) {
+        return taskService.updateTaskStatus(taskId, status);
     }
 }
