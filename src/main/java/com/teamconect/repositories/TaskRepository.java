41dabc5a8e0de611bcedee66927c87d82b4ac48b
SarAlbN1 <sara.albarracin.ns@gmail.com>
2024-10-27 12:07:13 -0500

    Configurar TaskRepository para manejo de tareas en MongoDB.
    Closes #34

--- src/main/java/com/teamconect/repositories/TaskRepository.java
+++ src/main/java/com/teamconect/repositories/TaskRepository.java
@@ -1,7 +1,9 @@
 package com.teamconect.repositories;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.teamconect.models.Task;
 
 public interface TaskRepository extends MongoRepository<Task, String> {
+    // Métodos personalizados de consulta (si es necesario)
 }
