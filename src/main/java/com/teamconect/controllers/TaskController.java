6d57896ee462d5c7f448f96d015655c850c44c51
SarAlbN1 <sara.albarracin.ns@gmail.com>
2024-10-27 12:08:39 -0500

    Crear endpoint en TaskController para asignación de tareas.
    Closes #37

--- src/main/java/com/teamconect/controllers/TaskController.java
+++ src/main/java/com/teamconect/controllers/TaskController.java
@@ -1,41 +1,25 @@
 package com.teamconect.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.teamconect.dtos.TaskDTO;
 import com.teamconect.models.Task;
 import com.teamconect.services.TaskService;
 
 @RestController
 @RequestMapping("/api/tasks")
 public class TaskController {
-    private final TaskService taskService;
 
     @Autowired
-    public TaskController(TaskService taskService) {
-        this.taskService = taskService;
-    }
+    private TaskService taskService;
 
     @PostMapping("/assign")
-    public ResponseEntity<String> assignTask(@RequestBody Task task) {
-        taskService.assignTask(task);
-        return new ResponseEntity<>("Tarea asignada con éxito", HttpStatus.OK);
+    public Task assignTask(@RequestBody TaskDTO taskDTO) {
+        Task task = new Task(taskDTO.getTitle(), taskDTO.getDescription(), taskDTO.getAssignedUserId(), taskDTO.getStatus());
+        return taskService.assignTask(task);
     }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Task> getTaskById(@PathVariable String id) {
-        Task task = taskService.getTaskById(id);
-        return task != null ? new ResponseEntity<>(task, HttpStatus.OK)
-                            : new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    }
-
-  
-
 }
