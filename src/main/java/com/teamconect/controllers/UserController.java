4480fe2d4a4ac36ca1612c4a12bc802518d518b6
SarAlbN1 <sara.albarracin.ns@gmail.com>
2024-11-05 22:58:38 -0500

    Creación de endpoint en UserController para asignar roles y departamentos. close #17

--- src/main/java/com/teamconect/controllers/UserController.java
+++ src/main/java/com/teamconect/controllers/UserController.java
@@ -1,10 +1,18 @@
 package com.teamconect.controllers;
 
-import com.teamconect.dtos.UsuarioRolDTO;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.teamconect.dtos.UserDTO;
 import com.teamconect.models.User;
 import com.teamconect.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
@@ -15,12 +23,35 @@ public class UserController {
 
     @PostMapping("/register")
     public User registerUser(@RequestBody UserDTO userDTO) {
-        User user = new User(userDTO.getName(), userDTO.getEmail(), userDTO.getPassword());
+        User user = new User(
+                userDTO.getUserName(),
+                userDTO.getUserEmail(),
+                userDTO.getUserPassword(),
+                userDTO.getUserPhoneNumber()
+        );
         return userService.registerUser(user);
     }
 
     @PostMapping("/assign-role-department")
-    public User assignRoleAndDepartment(@RequestBody UsuarioRolDTO usuarioRolDTO) {
-        return userService.assignRoleAndDepartment(usuarioRolDTO);
+    public User assignRoleAndDepartment(@RequestBody UserDTO userDTO) {
+        return userService.assignRoleAndDepartment(
+                userDTO.getUserId(),
+                userDTO.getUserRole(),
+                userDTO.getUserDepartment()
+        );
+    }
+
+    @GetMapping("/all")
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PutMapping("/update-role-department")
+    public User updateUserRoleAndDepartment(@RequestBody UserDTO userDTO) {
+        return userService.assignRoleAndDepartment(
+                userDTO.getUserId(),
+                userDTO.getUserRole(),
+                userDTO.getUserDepartment()
+        );
     }
 }
